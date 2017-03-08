8c06445076f6cf0b2946460223938ab350a4e32a
akashmitra <akash.mitra@outlook.com>
2017-03-08 20:27:49 +0530

    app.js udpate

--- app.js
+++ app.js
@@ -1,51 +1,61 @@
 /* Importing Support APIs */
-var express = require('express'),
-    http = require('http'),
-    path = require('path'),
-    log4js = require('log4js');
 
-var app = express();
+(function () {
 
-/* Injecting the Helper Classes */
-var db = require('./models/db');
-var logger = require('./log');
+    var express = require('express'),
+        http = require('http'),
+        path = require('path'),
+        log4js = require('log4js');
 
-var resourceList = require('./helper/fetchResourceHelper');
-var surveyRouter = require('./helper/surveyRouterHelper');
-var reviewRouter = require('./helper/reviewRouterHelper')
+    var app = express();
 
-
-/*JS client side files has to be placed under a folder by name 'public' */
-app.use(express.static(path.join(__dirname, 'public')));
-/*to access the posted data from client using request body*/
-app.use(express.bodyParser());
+    /* Injecting the Helper Classes */
+    var db = require('./models/db');
+    var logger = require('./log');
 
 
-/* Service for Resource List */
-app.get('/getResourceList', function(req, res) {
-    res.setHeader('Content-Type', 'application/json');
-    resourcelist = resourceList.getResource(req, res);
-    return resourcelist
-});
-
-/* Service to get feedback */
-app.post('/getReview', function(req, res) {
-    res.setHeader('Content-Type', 'application/json');
-    logger.trace('The Body :: ' + req.body);
-    feedbacklist = reviewRouter.fetchReview(req, res);
-    return feedbacklist
-});
-
-/* Service for Survey Submit */
-app.post('/submitSurvey', function(req, res) {
-    res.setHeader('Content-Type', 'application/json');
-    logger.trace('The Body :: ' + req.body);
-    surveysub = surveyRouter.answerrouter(req, res);
-    return surveysub
-});
+    var resourceList = require('./helper/fetchResourceHelper');
+    var surveyRouter = require('./helper/surveyRouterHelper');
+    var reviewRouter = require('./helper/reviewRouterHelper')
 
 
-/* Server at 80 */
-http.createServer(app).listen(80, function() {
-    logger.trace("Express server listening on port 80");
-});
+    /*JS client side files has to be placed under a folder by name 'public' */
+    app.use(express.static(path.join(__dirname, 'public')));
+    /*to access the posted data from client using request body*/
+    app.use(express.bodyParser());
+
+
+    /* Service for Resource List */
+    app.get('/getResourceList', function (req, res) {
+        res.setHeader('Content-Type', 'application/json');
+        resourcelist = resourceList.getResource(req, res);
+        return resourcelist
+    });
+
+    /* Service to get feedback */
+    app.post('/getReview', function (req, res) {
+        res.setHeader('Content-Type', 'application/json');
+        logger.trace('The Body :: ' + req.body);
+        feedbacklist = reviewRouter.fetchReview(req, res);
+        return feedbacklist
+    });
+
+    /* Service for Survey Submit */
+    app.post('/submitSurvey', function (req, res) {
+        res.setHeader('Content-Type', 'application/json');
+        logger.trace('The Body :: ' + req.body);
+        surveysub = surveyRouter.answerrouter(req, res);
+        return surveysub
+    });
+
+
+    /* Server at 80 */
+    http.createServer(app).listen(80, function () {
+        logger.trace("Express server listening on port 80");
+    });
+
+
+}());
+
+
+
