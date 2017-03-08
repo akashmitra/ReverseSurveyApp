c61142d85c1d7cfd984c2a52c5cfd0f0e8f0a25d
akashmitra <akash.mitra@outlook.com>
2017-03-08 20:32:16 +0530

    minor text fixes

--- models/db.js
+++ models/db.js
@@ -1,39 +1,39 @@
 // Bring Mongoose into the app 
-var mongoose = require( 'mongoose' ); 
+var mongoose = require('mongoose');
 
 // Build the connection string 
-var dbURI = 'mongodb://localhost:27017/v360'; 
+var dbURI = 'mongodb://localhost:27017/v360';
 
 // Create the database connection 
-mongoose.connect(dbURI); 
+mongoose.connect(dbURI);
 
 // CONNECTION EVENTS
 // When successfully connected
-mongoose.connection.on('connected', function () {  
-  console.log('Mongoose default connection open to ' + dbURI);
-}); 
+mongoose.connection.on('connected', function () {
+    console.log('Mongoose default connection open to ' + dbURI);
+});
 
 // If the connection throws an error
-mongoose.connection.on('error',function (err) {  
-  console.log('Mongoose default connection error: ' + err);
-}); 
+mongoose.connection.on('error', function (err) {
+    console.log('Mongoose default connection error: ' + err);
+});
 
 // When the connection is disconnected
-mongoose.connection.on('disconnected', function () {  
-  console.log('Mongoose default connection disconnected'); 
+mongoose.connection.on('disconnected', function () {
+    console.log('Mongoose default connection disconnected');
 });
 
 // If the Node process ends, close the Mongoose connection 
-process.on('SIGINT', function() {  
-  mongoose.connection.close(function () { 
-    console.log('Mongoose default connection disconnected through app termination'); 
-    process.exit(0); 
-  }); 
-}); 
+process.on('SIGINT', function () {
+    mongoose.connection.close(function () {
+        console.log('Mongoose default connection disconnected through app termination');
+        process.exit(0);
+    });
+});
 
 // BRING IN YOUR SCHEMAS & MODELS // For example 
 require('./resourcemodels');
-require('./overviewmodels');  
-require('./supervisormodels');  
+require('./overviewmodels');
+require('./supervisormodels');
 require('./teammodels');
 require('./portfoliomodels');  
